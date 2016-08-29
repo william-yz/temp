2bca7a10f3e74017189a8d0e0b9e904ff9345453
William Yang <william.yang@oocl.com>
2016-08-29 17:30:14 +0800

    add chat

---
+++ chat/client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+import MainLayout from '../layouts/MainLayout/MainLayout';
+
+const App = ({ location }) => {
+  return (
+    <MainLayout />
+  );
+};
+
+App.propTypes = {
+};
+
+export default App;
